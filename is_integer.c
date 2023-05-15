cce5fa1e5d707bd11db61ba95d685caf830acebe
azizios011 <hdriaziz699@gmail.com>
2023-05-15 03:01:28 -1100

    integer function

---
+++ is_integer.c
@@ -0,0 +1,25 @@
+#include "monty.h"
+/**
+ * is_integer - checks if a string is a valid integer
+ * @str: the string to check
+ *
+ * Return: 1 if str is a valid integer, 0 otherwise
+ */
+int is_integer(char *str)
+{
+	int i = 0;
+
+	if (str == NULL || *str == '\0')
+		return (0);
+
+	if (*str == '-')
+		i++;
+
+	for (; str[i] != '\0'; i++)
+	{
+		if (!isdigit(str[i]))
+			return (0);
+	}
+
+	return (1);
+}
