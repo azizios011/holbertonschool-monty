38d87f67c524f96b77d51f1e23a5f408fbadcdc9
azizios011 <hdriaziz699@gmail.com>
2023-05-15 03:06:09 -1100

    push function

---
+++ push.c
@@ -0,0 +1,26 @@
+#include "monty.h"
+
+/**
+ * push - Pushes an element to the stack
+ * @stack: Double pointer to the head of the stack
+ * @line_number: Line number being executed from script file
+ */
+void push(stack_t **stack, unsigned int line_number)
+{
+	char *n = strtok(NULL, " \n\t");
+	int num;
+
+	if (!n || !isdigit(*n))
+	{
+		fprintf(stderr, "L%u: usage: push integer\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	num = atoi(n);
+	if (add_node(stack, num) == NULL)
+	{
+		fprintf(stderr, "Error: malloc failed\n");
+		free_stack(*stack);
+		exit(EXIT_FAILURE);
+	}
+}
