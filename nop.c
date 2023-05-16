5f83a9be183f13a035d5f5dc78dcf327b72d1cf5
azizios011 <hdriaziz699@gmail.com>
2023-05-16 02:07:39 -1100

    nop function

---
+++ nop.c
@@ -0,0 +1,14 @@
+#include "monty.h"
+
+/**
+ * nop - Doesn't do anything.
+ * @stack: A pointer to the head of the stack.
+ * @line_number: The current line number.
+ *
+ * Return: void.
+ */
+void nop(stack_t **stack, unsigned int line_number)
+{
+	(void)stack;
+	(void)line_number;
+}
