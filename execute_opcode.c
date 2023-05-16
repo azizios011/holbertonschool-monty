af9c2c9e4c31327f4a454359feb6377c6ebd78d0
azizios011 <hdriaziz699@gmail.com>
2023-05-16 01:14:47 -1100

    execute opcode function

---
+++ execute_opcode.c
@@ -0,0 +1,36 @@
+#include "monty.h"
+
+/**
+ * execute_opcode - Executes the opcode using the appropriate function.
+ * @opcode: The opcode as a string.
+ * @stack: A pointer to the head of the stack.
+ * @line_number: The current line number.
+ *
+ * Return: void.
+ */
+void execute_opcode(char *opcode, stack_t **stack, unsigned int line_number)
+{
+	instruction_t opcodes[] = {
+		{"push", push},
+		{"pall", pall},
+		{"pint", pint},
+		{"pop", pop},
+		{"swap", swap},
+		{"add", add},
+		{NULL, NULL}
+	};
+	int i = 0;
+
+	while (opcodes[i].opcode)
+	{
+		if (strcmp(opcode, opcodes[i].opcode) == 0)
+		{
+			opcodes[i].f(stack, line_number);
+			return;
+		}
+		i++;
+	}
+	fprintf(stderr, "L%u: unknown instruction %s\n", line_number, opcode);
+	free_stack(*stack);
+	exit(EXIT_FAILURE);
+}
