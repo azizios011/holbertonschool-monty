e0ceac235a59926118fc12cbb79bf94a8da6b511
azizios011 <hdriaziz699@gmail.com>
2023-05-17 22:23:17 -1100

    monty heade file

--- monty.h
+++ monty.h
@@ -44,6 +44,8 @@ void pop(stack_t **stack, unsigned int line_number);
 void swap(stack_t **stack, unsigned int line_number);
 void add(stack_t **stack, unsigned int line_number);
 void nop(stack_t **stack, unsigned int line_number);
+void sub(stack_t **stack, unsigned int line_number);
+
 
 /*Helper_Functions_ProtoTypes*/
 stack_t *add_node(stack_t **stack, int n);
